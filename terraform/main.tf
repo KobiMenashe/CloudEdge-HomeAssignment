c700a2894da24645b893619a44f94ddcb99c0e5f
Kobi <kobimenashe7@gmail.com>
2023-09-23 22:53:18 +0300

    deploy vnet & subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,13 +28,13 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "kobi-vnet"
   address_space       = ["192.168.0.0/16"]
   location            = "westeurope"
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
 
 # Create Subnet
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["192.168.1.0/24"]
 }
