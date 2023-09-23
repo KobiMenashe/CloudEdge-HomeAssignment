ef24e07a7e390d32728d467522f55ce7bebe1018
Kobi <kobimenashe7@gmail.com>
2023-09-23 22:43:56 +0300

    deploy vnet & subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,6 +11,7 @@ provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x.
   # If you're using version 1.x, the "features" block is not allowed.
   version = "~>2.76.0"
+  skip_provider_registration = true
   features {}
 }
 
