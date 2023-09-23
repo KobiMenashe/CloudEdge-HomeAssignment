1bc0c2223fd0bbef8ee37c5b48affe3e1701d1f6
Kobi <kobimenashe7@gmail.com>
2023-09-23 22:15:28 +0300

    deploy vnet & aks

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,43 +1,43 @@
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "KobiAssignment"
-#     storage_account_name = "sadevweu01"
-#     container_name       = "tfstate"
-#     key                  = "terraform.tfstate"
-#   }
-# }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "KobiAssignment"
+    storage_account_name = "sadevweu01"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
  
-# provider "azurerm" {
-#   # The "feature" block is required for AzureRM provider 2.x.
-#   # If you're using version 1.x, the "features" block is not allowed.
-#   version = "~>2.0"
-#   features {}
-# }
+provider "azurerm" {
+  # The "feature" block is required for AzureRM provider 2.x.
+  # If you're using version 1.x, the "features" block is not allowed.
+  version = "~>2.0"
+  features {}
+}
  
-# data "azurerm_client_config" "current" {}
+data "azurerm_client_config" "current" {}
  
 
 data "azurerm_resource_group" "rg" {
   name = "KobiAssignment"
 }
 
-# Create AKS
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "kobi-aks"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  dns_prefix          = "kobiaks1"
+# # Create AKS
+# resource "azurerm_kubernetes_cluster" "aks" {
+#   name                = "kobi-aks"
+#   location            = data.azurerm_resource_group.rg.location
+#   resource_group_name = data.azurerm_resource_group.rg.name
+#   dns_prefix          = "kobiaks1"
 
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_A2_v2"
-  }
+#   default_node_pool {
+#     name       = "default"
+#     node_count = 1
+#     vm_size    = "Standard_A2_v2"
+#   }
 
-  identity {
-    type = "SystemAssigned"
-  }
-}
+#   identity {
+#     type = "SystemAssigned"
+#   }
+# }
 
 # Create Virtual Network
 resource "azurerm_virtual_network" "hub_vnet" {
