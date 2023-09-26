f5e85881ded160d7d7fd723517eae3bc99c4daca
Kobi <kobimenashe7@gmail.com>
2023-09-26 16:40:06 +0300

    add kv

--- terraform/main.tf
+++ terraform/main.tf
@@ -73,14 +73,15 @@ resource "azurerm_key_vault" "kv" {
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
-  sku_name = "standard"
-  access_policy {
+  sku_name                    = "standard"
+
+    access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
     key_permissions = [
       "Get",
     ]
-    
+
     secret_permissions = [
       "Get",
     ]
