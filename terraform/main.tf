8ef99b2e48bfd00e5e98d7389dcfb2cbdd495677
Kobi <kobimenashe7@gmail.com>
2023-09-26 16:34:29 +0300

    add kv

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,6 +37,15 @@ resource "azurerm_subnet" "hub_subnet" {
   address_prefixes     = ["192.168.1.0/24"]
 }
 
+# Create Bastion Subnet
+resource "azurerm_subnet" "AzureBastion_subnet" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.hub_vnet.name
+  address_prefixes     = ["192.168.2.0/26"]
+}
+
+
 # Create AKS
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "kobi-aks"
@@ -54,3 +63,94 @@ resource "azurerm_kubernetes_cluster" "aks" {
     type = "SystemAssigned"
   }
 }
+
+# Create Key Vault
+resource "azurerm_key_vault" "kv" {
+  name                        = "kobivault"
+  location                    = data.azurerm_resource_group.rg.location
+  resource_group_name         = data.azurerm_resource_group.rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  sku_name = "standard"
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+    key_permissions = [
+      "Get",
+    ]
+    secret_permissions = [
+      "Get",
+    ]
+    storage_permissions = [
+      "Get",
+    ]
+  }
+}
+
+# data "azurerm_key_vault_secret" "kobi_pass_secret" {
+#   name         = "KobiPassVM"
+#   key_vault_id = data.azurerm_key_vault.kv.id
+# }
+
+# # Create Bastion
+# resource "azurerm_bastion_host" "BastionSubnet" {
+#   name                = "AzureBastionSubnet"
+#   location            = data.azurerm_resource_group.rg.location
+#   resource_group_name = data.azurerm_resource_group.rg.name
+
+#   ip_configuration {
+#     name                 = "configuration"
+#     subnet_id            = azurerm_subnet.hub_subnet.id
+#     public_ip_address_id = azurerm_public_ip.vm_pip.id
+#   }
+# }
+
+# # Create Network Interface
+# resource "azurerm_network_interface" "vm_interface" {
+#   name                = "kobivm-nic"
+#   location            = data.azurerm_resource_group.rg.location
+#   resource_group_name = data.azurerm_resource_group.rg.name
+
+#   ip_configuration {
+#     name                          = "configuration1"
+#     subnet_id                     = azurerm_subnet.hub_subnet.id
+#     private_ip_address_allocation = "Static"
+#     private_ip_address            = "192.168.1.8"
+#   }
+# }
+
+# # Create PIP
+# resource "azurerm_public_ip" "vm_pip" {
+#   name                = "kobi-vm-pip"
+#   resource_group_name = data.azurerm_resource_group.rg.name
+#   location            = data.azurerm_resource_group.rg.location
+#   allocation_method   = "Static"
+#   sku                 = "Standard"
+# }
+
+# # Create VM
+# resource "azurerm_windows_virtual_machine" "vm" {
+#   name                = "kobi-vm"
+#   resource_group_name = data.azurerm_resource_group.rg.name
+#   location            = data.azurerm_resource_group.rg.location
+#   size                = "Standard_F2"
+#   admin_username      = "kobi"
+#   admin_password      = data.azurerm_key_vault_secret.kobi_pass_secret.value
+#   disable_password_authentication = "false"
+#   network_interface_ids = [
+#     azurerm_network_interface.vm_interface.id,
+#   ]
+#   os_disk {
+#     caching              = "ReadWrite"
+#     storage_account_type = "Standard_LRS"
+#   }
+
+#   source_image_reference {
+#     publisher = "MicrosoftWindowsServer"
+#     offer     = "WindowsServer"
+#     sku       = "2016-Datacenter"
+#     version   = "latest"
+#   }
+# }
