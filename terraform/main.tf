7f9b01ea1fde598c644a66dad2f908348c9855ad
Kobi <kobimenashe7@gmail.com>
2023-09-26 16:51:08 +0300

    add kv

--- terraform/main.tf
+++ terraform/main.tf
@@ -74,22 +74,6 @@ resource "azurerm_key_vault" "kv" {
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
   sku_name                    = "standard"
-
-    access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-    key_permissions = [
-      "Get",
-    ]
-
-    secret_permissions = [
-      "Get",
-    ]
-
-    storage_permissions = [
-      "Get",
-    ]
-  }
 }
 
 # data "azurerm_key_vault_secret" "kobi_pass_secret" {
